1ca92013e23d7a6efd012b6930ce3c896ad9e0c1
Bezlonir <michael_bragg99@hotmail.com>
2018-11-19 11:26:28 -0700

    Complete methods for displaying blocks and style background.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,31 @@
+// Model for game calculations
 const gameController = (function(){
+    // array to hold all level objects
     const levels = [];
-    const blocks = [];
+    
+    // game state object
+    const game = {
+        started: true,
+        level: 0,
+        hasChanged: true
+    }
 
+    // Block function constructor
+    const Block = function(width, hp, density, type) {
+        this.width = width;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.density = density;
+        this.type = type
+    };
+
+    // Base Level Size
     let levelSize = {
         x: 640,
         y: 480
     }
 
+    // ball object
     let ball = {
         position: {
             x: 200,
@@ -19,7 +38,8 @@ const gameController = (function(){
         },
         
         size: 6,
-        damage: 3
+        damage: 3,
+        maxSpeed: 4
     }
 
     return {
@@ -47,7 +67,80 @@ const gameController = (function(){
 
         getLevelSize: function() {
             return levelSize;
+        },
+
+        getLevelSizeInCells: function() {
+            return {y: levels[game.level].length,
+                    x: levels[game.level][0].length};
+        },
+
+        createBasicLevel: function() {
+            let tLevel = [];
+            const columns = Math.floor(levelSize.x / 36);
+            const rows = Math.floor(levelSize.y / 18);
+            
+            // iterate through the rows and columns and
+            // populate an area of blocks with boolean false
+            // elsewise
+            for (let row = 0; row < rows; row++) {
+                let cellsRow = [];
+                for (let col = 0; col < columns; col++) {
+                    if (row > 2 && row < 10) {
+                        if (col > 2 && col < columns - 2) {
+                            let tBlock = new Block(1, 10, 1, 'basic');
+                            cellsRow.push(tBlock);
+                        } else {
+                            cellsRow.push(false);
+                        }
+                    } else {
+                        cellsRow.push(false);
+                    }
+                }
+
+                tLevel.push(cellsRow);
+            }
+            
+            levels.push(tLevel);
+            //return tLevel;
+        },
+
+        getCell: function(x, y) {
+            return levels[game.level][x][y];
+        },
+
+        getLevelObjectForUI: function() {
+            let levelT = [];
+            levels[game.level].forEach((row) => {
+               let rowT = row.map((cell) => {
+                    if (cell) {
+                        return {
+                            width: cell.width,
+                            type: cell.type,
+                            opacity: Math.floor(((cell.hp/ cell.maxHp) * 70) + 30)
+                        }
+                    } else {
+                        return false;
+                    }
+               });
+               levelT.push(rowT);
+            });
+
+            return levelT;
+        },
+
+        getLevelState: function() {
+            return game.hasChanged;
+        },
+
+        setLevelState: function(state) {
+            game.hasChanged = state;
+        },
+
+        test: function() {
+            console.log(levels[game.level]);
         }
+
+
 
         
     }
@@ -59,24 +152,57 @@ const UIController = (function(){
         canvas: `#myCanvas`
     };
 
+    const levelThemes = [
+        {
+            basic: `rgba(80, 100, 140, %alpha)`
+        }
+    ];
+    
+    let currentLevel = [];
+
     drawRect = function(ctx, fill, x, y, h, w) {
         ctx.beginPath();
-        ctx.rect(y, x, y + h, x + w);
+        ctx.rect(x, y, w, h);
         ctx.fillStyle = fill;
         ctx.fill();
         ctx.closePath();
     }
-
-    
 
     return {
         getDomStrings: function() {
            return DOMStrings;
         },
 
-        drawCanvas: function(CTX) {
-            drawRect(CTX, `#FF0000`, 20, 40, 30, 30);
-            drawCircle(CTX, `green`, 240, 160, 20);
+        drawCanvas: function(CTX, x, y) {
+            
+            const canvasRef = document.querySelector(DOMStrings.canvas);
+            
+
+            for (let col = 0; col < currentLevel.length; col++) {
+                for (let row = 0; row < currentLevel[0].length; row++) {
+                    const posThis = {
+                        x: Math.floor((canvasRef.width / (currentLevel[0].length + 1)) * row),
+                        y: Math.floor((canvasRef.height / (currentLevel.length + 1)) * col)
+                    };
+
+                    
+                    if (currentLevel[col][row]) {
+                        const colorT = levelThemes[0].basic.replace('%alpha', (currentLevel[col][row].opacity / 100).toString());
+                        
+                        drawRect(CTX,
+                            colorT,
+                            posThis.x,
+                            posThis.y,
+                            8,
+                            Math.floor((currentLevel[col][row].width * ((640 / currentLevel[0].length) * .8))),
+                            );
+                    }
+                }
+                
+            }
+            
+            //drawRect(CTX, `#FF0000`, 20, 40, 30, 30);
+            
             // CTX.beginPath();
             // CTX.rect(20, 40, 50, 50);
             // CTX.fillStyle = "#FF0000";
@@ -90,7 +216,22 @@ const UIController = (function(){
             ctx.fillStyle = fill;
             ctx.fill();
             ctx.closePath();
+        },
+
+        // populates the currentLevel object in the
+        // UIController so that level objects can be
+        // rendered independently of the Model without
+        // sending too much information to the
+        // UI module
+        setCurrentLevel: function(level) {
+            currentLevel = level;
+        },
+
+        test: function() {
+            console.table(currentLevel);
         }
+
+        
     }
 })();
 
@@ -107,6 +248,8 @@ const Controller = (function(gameCtrl, UICtrl){
         const DOM = UICtrl.getDomStrings();
         const mCanvas = document.querySelector(DOM.canvas);
 
+
+        gameCtrl.createBasicLevel();
         gameCtrl.setBallPos(mCanvas.width /2, mCanvas.height -30);
     }
 
@@ -120,15 +263,29 @@ const Controller = (function(gameCtrl, UICtrl){
             ball.size);
     }
 
-    // draw the game area, called by setInterval
-    drawGame = function() {
+    // handle an update frame called by setInterval
+    update = function() {
+        // link to the Canvas DOM object
         const DOM = UICtrl.getDomStrings();
         const mCanvas = document.querySelector(DOM.canvas);
         const ctx = mCanvas.getContext("2d");
 
+        // get information from the game controller
+        // about the ball
         const ball = gameCtrl.getBall();
 
+        // clear the canvas
         ctx.clearRect(0,0, mCanvas.width, mCanvas.height);
+
+        // check if the state of the level has changed
+        const needsUpdate = gameCtrl.getLevelState();
+        if (needsUpdate) {
+            UICtrl.setCurrentLevel(gameCtrl.getLevelObjectForUI());
+            gameCtrl.setLevelState(false);
+        }
+
+        const size = gameCtrl.getLevelSizeInCells();
+        UICtrl.drawCanvas(ctx, size.x, size.y);
 
         drawBall(ctx, ball);
 
@@ -144,7 +301,7 @@ const Controller = (function(gameCtrl, UICtrl){
             // set the starting conditions for a game
             startGame();
 
-            setInterval(drawGame, 10);
+            setInterval(update, 10);
             
         }
     }
