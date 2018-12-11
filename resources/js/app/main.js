db7ea9128f198cad7e3bec9f2fd877584870ead5
Bezlonir <michael_bragg99@hotmail.com>
2018-12-10 19:46:05 -0700

    Fix syntax error.

--- resources/js/app/main.js
+++ resources/js/app/main.js
@@ -714,7 +714,7 @@ const UIController = (function(){
                 tLifeView.style.height = paddle.size.y;
                 tLifeView.style.backgroundColor = paddle.color;
                 console.log(tLifeView);
-                LivesView.innerHTML.appendChild(tLifeView);
+                LivesView.appendChild(tLifeView);
                 console.log(LivesView);
             }
 
